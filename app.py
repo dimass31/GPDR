dba4dd037fbf5144199980b5301df2e39d07f62b
dmitriy <nikitenkovdmitriy@yandex.ru>
2020-04-04 19:28:27 +0300

    add two page

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask, request, redirect, render_template
+from flask import Flask, request, redirect, render_template, url_for
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('home.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    if request.method == 'POST':
+        login = request.form.get('login')
+        password = request.form.get('password')
+        return redirect(url_for('index'))
+    return render_template('login.html')
+
+@app.route('/signup')
+def signup():
+    return render_template('signup.html')
 
 app.run(debug=True)
