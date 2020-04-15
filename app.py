0740deb58fdb6f8ac186f1242f21008b88ca37c1
dmitriy <nikitenkovdmitriy@yandex.ru>
2020-04-15 23:52:57 +0300

    editing

--- app.py
+++ app.py
@@ -4,14 +4,23 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
-    return render_template('home.html')
+    if request.method == 'POST':
+        print('post')
+    if request.method == 'GET':
+        print('get')
+        try:
+            login = request.args['login']
+            print(login)
+            return render_template('home.html', login=login)
+        except:
+            return render_template('home.html')
 
 @app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         login = request.form.get('login')
         password = request.form.get('password')
-        return redirect(url_for('signup', login=login))
+        return redirect(url_for('index', login=login))
     return render_template('login.html')
 
 @app.route('/signup')
