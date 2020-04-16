f6c75574133dd285015139fa89318bf3941d39f7
dmitriy <nikitenkovdmitriy@yandex.ru>
2020-04-16 16:30:50 +0300

    add redirect to profile

--- app.py
+++ app.py
@@ -5,9 +5,9 @@ app = Flask(__name__)
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
-        print('post')
+        pass
     if request.method == 'GET':
-        print('get')
+        pass
         try:
             login = request.args['login']
             print(login)
@@ -23,8 +23,11 @@ def login():
         return redirect(url_for('index', login=login))
     return render_template('login.html')
 
-@app.route('/signup')
+@app.route('/signup', methods=['POST', 'GET'])
 def signup():
+    if request.method == 'POST':
+        login = request.form.get('login')
+        return redirect(url_for('index', login=login))
     return render_template('signup.html')
 
 @app.route('/profile')
